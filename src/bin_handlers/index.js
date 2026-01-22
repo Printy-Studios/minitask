088623bc7a058e64658c54206e7192f531f6c9fa
Jorens <jorensmerenjanu@gmail.com>
2026-01-22 06:12:28 +0200

    docs: add comment to bin_handlers/index.js

--- src/bin_handlers/index.js
+++ src/bin_handlers/index.js
@@ -4,6 +4,17 @@ import * as view from './view.js';
 import * as list from './list.js';
 import * as update from './update.js';
 
+/**
+ * Handlers for commands
+ * Each command has a dedicated handler with its own module,
+ * At the least, a command handler should export a `command` string
+ * and a `handler` function.
+ * 
+ * See more info at: https://github.com/yargs/yargs/blob/HEAD/docs/advanced.md#providing-a-command-module
+ * 
+ * TODO: Decouple command handling from Yargs.
+ */
+
 const commands = [
     _new,
     init,
